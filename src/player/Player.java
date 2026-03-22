36c420ca37568099865c19d3d4f37ebf515c9e0e
christian corona <cac620@txstate.edu>
2026-03-21 22:54:07 -0500

    Added player chess

---
+++ src/player/Player.java
@@ -0,0 +1,14 @@
+package player;
+
+public class Player {
+	private String name;
+	private String color;
+
+	public Player(String name, String color) {
+		this.name = name;
+		this.color = color;
+	}
+	public String getName() { return name; }
+	public String getColor() { return color; }
+
+}
