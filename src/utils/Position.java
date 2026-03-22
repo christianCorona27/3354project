aa1a9fb7dc499c42bc07cb9e6817b9e456d785c9
christian corona <cac620@txstate.edu>
2026-03-21 22:48:27 -0500

    Added Position class

---
+++ src/utils/Position.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class Position {
+	private int row;
+	private int col;
+
+	public Positions(int row, int col){
+		this.row = row;
+		this.col = col;
+	}
+	public int getRow() { return row; }
+	public int getCol() { return col; }
+
+	public void setRow(int row) { this.row = row; }
+	public void setCol(int col) { this.col = col; }
+}
